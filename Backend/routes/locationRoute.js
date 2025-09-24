25c3a9cb2615ac85f8f74bbeaf812c79c22029c1
Trần Quốc Huy <104780877+qhuy1504@users.noreply.github.com>
2025-09-24 08:07:45 +0700

    24/09 update GD admin quản lý bài đăng

--- Backend/routes/locationRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getLocationFromCoords, getNearbyHostels } from "../controllers/locationController.js";
-
-const router = express.Router();
-
-router.post("/", getLocationFromCoords);
-router.post("/nearby-hostels", getNearbyHostels);
-
-export default router;
