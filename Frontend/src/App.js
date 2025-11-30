99dc9ae196cf6e9fbaffbeb2c382833dbac2564a
Trần Quốc Huy <104780877+qhuy1504@users.noreply.github.com>
2025-11-30 16:12:49 +0700

    30/11 thêm dockerfile

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,9 +8,15 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import ViewTrackingManager from "./components/common/ViewTrackingManager"
 import "./styles/themes.css"
 
+const GOOGLE_CLIENT_ID =
+  process.env.REACT_APP_GOOGLE_CLIENT_ID ||
+  "522368004135-fsujl8qhea0i433i3n766a9stpggdeme.apps.googleusercontent.com"
+
 function App() {
+  console.log("Google Client ID (frontend):", GOOGLE_CLIENT_ID)
+
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <ThemeProvider>
         <AuthProvider>
           <FavoritesProvider>
